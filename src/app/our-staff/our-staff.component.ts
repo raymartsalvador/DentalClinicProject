3a1f8c11f17081d29d008c49925e8d5f11cb5252
4R3M <salvadorraymartg@gmail.com>
2023-04-25 01:48:08 +0800

    added nav-bar

---
+++ src/app/our-staff/our-staff.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-staff',
+  templateUrl: './our-staff.component.html',
+  styleUrls: ['./our-staff.component.scss']
+})
+export class OurStaffComponent {
+
+}
