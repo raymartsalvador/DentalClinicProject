cb6fdb727e74b235bfdb24beb4dfd732264896cf
4R3M <salvadorraymartg@gmail.com>
2023-04-25 21:16:23 +0800

    changed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
 ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'lightBlue': '#00b2bf',
+      'LighterBlue': '#40e2dd',
+      'black': '#18191f',
+      'white' :'#f2f2f2',
+      'greenishBlue': '#9bdad9'
+      },
   },
   plugins: [ require('flowbite/plugin')],
 }
