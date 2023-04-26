6aba0e1da758a92635f5ffaaa90be1258b7fc273
JohannahG <111161441+JohannahG@users.noreply.github.com>
2023-04-27 02:38:46 +0800

    contact us,routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { OurStaffComponent } from './our-staff/our-staff.component';
 const routes: Routes = [
   {path:"" ,component: HomeComponent},
   {path:"home" ,component: HomeComponent},
-  {path:"home" ,component: HomeComponent},
-  {path:"home" ,component: HomeComponent},
+  {path:"about" ,component: AboutComponent},
+  {path:"contact" ,component: ContactUsComponent},
+  {path:"staff" ,component: OurStaffComponent},
+
 ];
 
 @NgModule({
