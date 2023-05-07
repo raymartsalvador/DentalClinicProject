754c454c28f767ff454b3f9656ca749b3f658c40
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-05-07 15:42:54 +0800

    feat(server): :white_check_mark: added login api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,22 +1,53 @@
-const express = require('express')
-const router = express.Router()
-
-const mongoose = require('mongoose')
-const db = "mongodb+srv://user:E1l79iXG86enMmyL@cluster0.yuik2c0.mongodb.net/"
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const mongoose = require("mongoose");
+const db =
+  "mongodb+srv://user:E1l79iXG86enMmyL@cluster0.yuik2c0.mongodb.net/DentalClinic";
 
 async function connect() {
   try {
-    await mongoose.connect(db)
-    console.log('Connected to mongodb')
+    await mongoose.connect(db);
+    console.log("Connected to mongodb");
   } catch (err) {
-    console.error('Error connecting to mongodb', err)
+    console.error("Error connecting to mongodb", err);
   }
 }
 
-connect()
+connect();
 
-router.get('/', (req, res)=>{
-  res.send('From API route')
-})
+router.get("/", (req, res) => {
+  res.send("From API route");
+});
+
+router.post("/register", async (req, res) => {
+  let userData = req.body;
+  let user = new User(userData);
+  try {
+    const registeredUser = await user.save();
+    res.status(200).send(registeredUser);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  let userData = req.body;
+  try {
+    let user = await User.findOne({ email: userData.email }).exec();
+    if (!user) {
+      res.status(401).send("invalid email");
+    } else if (user.password !== userData.password) {
+      res.status(401).send("invalid password");
+    } else {
+      res.status(200).send(user);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
+
 
 module.exports = router;
