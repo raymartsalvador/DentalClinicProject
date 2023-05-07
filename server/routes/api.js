6068ed9e7c49c5044804bfdba68a6fb8511c440c
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-05-07 14:14:51 +0800

    feat(root): :white_check_mark: added mongodb

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const router = express.Router()
+
+const mongoose = require('mongoose')
+const db = "mongodb+srv://user:E1l79iXG86enMmyL@cluster0.yuik2c0.mongodb.net/"
+
+async function connect() {
+  try {
+    await mongoose.connect(db)
+    console.log('Connected to mongodb')
+  } catch (err) {
+    console.error('Error connecting to mongodb', err)
+  }
+}
+
+connect()
+
 router.get('/', (req, res)=>{
   res.send('From API route')
 })
 
 module.exports = router;
-
-console.log();
