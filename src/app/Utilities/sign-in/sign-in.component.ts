8d6b5417613526128ae99d9583a3a7dead4771f2
JohannahG <111161441+JohannahG@users.noreply.github.com>
2023-05-12 17:49:51 +0800

    sign

--- src/app/Utilities/sign-in/sign-in.component.ts
+++ src/app/Utilities/sign-in/sign-in.component.ts
@@ -23,20 +23,8 @@ export class SignInComponent implements OnInit {
     );
   }
 
-<<<<<<< HEAD
   onPressingForgotPassword() {}
   onPressingSignUp() {}
-=======
-
-
-
-  onPressingForgotPassword() {
-    this._router.navigate(['/forgot-password'])
-  }
-  onPressingSignUp() {
-    this._router.navigate(['/signUp'])
-  }
->>>>>>> df9527a526238f1d309da710fffc79ada0c9ef7b
 
   ngOnInit(): void {}
 }
