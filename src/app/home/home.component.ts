8c549c6c3f37262e250d6e6d6d428134a9cd9b63
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-05-14 04:10:34 +0800

    optimized public components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServiceCrudService } from '../services/service-crud.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+  services: any[] = [];
+  constructor(
+    private _SERVICECRUD: ServiceCrudService,
+    public _ROUTER: Router
+  ) {}
+  ngOnInit(): void {
+    this.getServices()
+  }
+  getServices(): void {
+    this._SERVICECRUD.getServices().subscribe(
+      (response) => {
+        this.services = response;
+        console.log('Services:', this.services);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+  OnMakeAnAppointment() {
+    this._ROUTER.navigate(['/appointments']);
+  }
+  OnOurServices() {
+    this._ROUTER.navigate(['/services']);
+  }
 }
--- src/app/our-services/our-services.component.ts
+++ src/app/our-services/our-services.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceCrudService } from '../services/service-crud.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-our-services',
   templateUrl: './our-services.component.html',
-  styleUrls: ['./our-services.component.scss']
+  styleUrls: ['./our-services.component.scss'],
 })
-export class OurServicesComponent {
-
+export class OurServicesComponent implements OnInit {
+  services: any[] = [];
+  constructor(
+    private _SERVICECRUD: ServiceCrudService,
+    public _ROUTE: Router
+  ) {}
+  getServices(): void {
+    this._SERVICECRUD.getServices().subscribe(
+      (response) => {
+        this.services = response;
+        console.log('Services:', this.services);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+  OnMakeAppointment() {
+    this._ROUTE.navigate(['/appointments']);
+  }
+  ngOnInit(): void {
+    this.getServices();
+  }
 }
